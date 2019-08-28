16e1f76147eee9df3309d0213ed1da22c9d3f2a2
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-08-28 11:20:39 -0600

    Set up cards and refactor ideas

--- src/Ideas.js
+++ src/Ideas.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Ideas.css';
+import Card from './Card'
 
 const Ideas = (props) => {
-    const { name } = props
+    const { ideas } = props;
+
+    const ideaCards = ideas.map(idea => {
+        return (
+            <Card 
+                title={idea.title}
+                description = {idea.description}
+                id = {idea.id}
+                key = {idea.id}
+            />
+        )
+    })
     return (
-        <h2>Hey, {name}</h2>
+        <section className="ideas__section">
+            {ideaCards}
+        </section>
     )
 }
 
